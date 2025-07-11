999ef4b51481f584466a2f8860c8188045cc41ee
Praveen <praveen.k@xnara.com>
2025-07-11 22:06:04 +0530

    added loop

---
+++ loops.ts
@@ -0,0 +1,86 @@
+// Loops - Which will execute the same block of code {...} multiple times
+
+console.log(1)
+console.log(2)
+console.log(3)
+console.log(4)
+console.log(5)
+
+console.log("************************");
+
+// 1. for loop 
+    // a. for loop - Traditional for loop - If we know how many times we have to run the iteration
+    // b. for ...of loop - Loop over the iteratable object - Array - [1,2,3,4]
+    // c. for ...in loop - Looping over the properties of an object - 
+    // {key : value }
+// 2. while loop - If we do not know how many times we have to run the iteration
+// 3. do-while loop - If we want to execute to loop atleast one time before check ine the condition
+
+
+// 1. for loop
+// Syntax:
+
+/*
+for(initiliasation; condition; increment/decrement)
+{
+    // code
+    break
+}
+initiliasation - Initialise the value of the variable to start the execution. let i = 1
+condition - Condition to check whether the loop should continue or not. i < 5
+increment/decrement - Increment or decrement the value of the variable after each iteration. i++
+
+*/
+
+for(let i =1; i<6; i++){ // 1<6  1++ = 1+1 =2 , 2 < 6 3 5++ = 5+1 = 6
+    console.log(i) // 1 2 3 4 5
+    // if(i==3){
+    //     break
+    // }
+}
+
+console.log("********************************");
+
+// 2. while loop
+// Syntax:
+
+/*
+// initiliasation
+
+while(condition)
+{
+    // code
+    increment/Decrement
+}
+
+*/
+
+// 1....10
+let j = 1
+while(j<11){
+    console.log(j)
+    j++
+}
+
+console.log("***************************");
+
+// 3. do while loop
+// Syntax:
+
+/*
+
+initiliasation
+do{
+ // code
+ increment/decrement
+}while(condition);
+
+*/
+
+// 10 ... 1
+
+let k =10
+do{
+    console.log(k) // 10 
+  //  k--
+}while(k>=1); // k>0, k>=1, k!=0,
--- statements.ts
+++ statements.ts
@@ -1,4 +1,4 @@
-// Statement -  Allows us to execute different block of code {...} for different condition
+// Conditional Statement -  Allows us to execute different block of code {...} for different condition
 
 // 1. if statement
 // 2. if else statement
@@ -40,7 +40,7 @@ else
 
 */
 
-let age  = 15
+let age  = 25
 
 if(age > 18){ // {15 > 18} - false
     console.log("You are eligible for voting");
@@ -51,4 +51,175 @@ else
 }
 
 
-console.log("1(||)"+ ((1>2) || (5<19))) //- BODMAS
+// 3. if else if else
+// Syntax:
+
+/*
+
+if(condition)
+{
+// if condition is true - If block will be executed
+}
+else if(condition1)
+{
+// if condition1 is true - then this if block will be executed
+}
+else if(condition2)
+{
+// if condition2 is true - then this if block will be executed
+}
+else
+{
+// code
+}
+
+*/
+
+
+
+let browser = "edge"
+
+if(browser == "chrome"){
+    console.log("Launch the chrome browser")
+    //break
+}
+else if(browser == "firefox"){
+    console.log("Launch the firefox browser")
+}
+else if(browser == "safari"){
+    console.log("Launch the safari browser")
+}
+else{
+    console.log("Browser is Invalid");
+}
+
+
+// When to use if else if 
+// 1. Ranges (>90)
+// 2. Logical Operators && ||
+// 3. Multiple datatypes  for comparison  - name= "rahul", salary > 200000
+
+
+
+
+
+// 4. switch  statement
+// Syntax:
+
+/*
+switch(expression){
+    case "value1":
+        // code to be executed if expression matches value1
+        break
+    case value2:
+        // code to be executed if expression matches value2
+        break
+    default:
+        // code to be executed if expression does not match any of the values
+        break   
+}
+*/
+
+let browserName = "edge"
+
+switch(browserName){ // switch("chrome")
+    case "chrome":
+        console.log("Launch the chrome browser from switch")
+        break
+    case "firefox":
+        console.log("Launch the firefox browser from switch")
+        break
+    case "safari":
+        console.log("Launch the safari browser from switch")
+        break
+    default:
+        console.log("Browser is Invalid from switch");
+}
+
+
+let day = 1
+
+switch(day){
+    case 1:
+        console.log("Monday");
+        break;      
+}
+
+// 5. Nested if else
+// Syntax:
+
+/*
+if(condition1)
+{
+if(condition2)
+{
+// code to be executed if both conditions are true
+}
+else
+{
+// code to be executed if condition1 is true but condition2 is false
+}
+}
+else
+{
+// code to be executed if condition1 is false
+}
+
+*/
+
+let score = 49
+
+// below 50 - Failed
+// 50 to 69 - D
+// 70-79 - C
+// 80-89 - B
+// 90-100 - A
+
+if(score >=50){
+    if(score >=90){
+        console.log("Grade A");
+    }else{
+        if(score >= 80){
+            console.log("Grade B");
+        }else{
+            if(score >= 70){
+                console.log("Grade C");
+            }else{
+                console.log("Grade D");
+            }
+        }
+    }
+}
+else{
+    console.log("Failed");
+}
+
+
+
+// 6. Ternary operator
+// The ternary operator is a shorthand way of writing if-else statements. It consists of three
+// parts: the condition, the value if the condition is true, and the value if the condition is false
+// 2nd and 3rd will be separated with (:)
+
+
+// 1. condition?
+// 2. The result for if the condition is true
+// 3. The result for if the condition is false
+
+
+// let age  = 15
+
+// if(age > 18){ // {15 > 18} - false
+//     console.log("You are eligible for voting");
+// }
+// else
+// {
+//     console.log("You are not eligible for voting");
+// }
+
+
+let result = (age>=18)? "You are eligible for voting" : "You are not eligible for voting"
+console.log(result);
+
+
+// loops
