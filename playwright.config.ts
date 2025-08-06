51422153dac762653041ee4745f88866a4851056
Praveen <praveen.k@xnara.com>
2025-08-06 22:24:08 +0530

    ädded input field validation

--- playwright.config.ts
+++ playwright.config.ts
@@ -28,6 +28,10 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html', // allure report . Monocart report
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  // timeout : 60000,
+  // expect:{
+  //   timeout:10000
+  // },
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
@@ -46,35 +50,35 @@ export default defineConfig({
       use: { ...devices['Desktop Chrome'] },
     },
 
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
+    // {
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox'] },
+    // },
 
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
+    // {
+    //   name: 'webkit',
+    //   use: { ...devices['Desktop Safari'] },
+    // },
 
     /* Test against mobile viewports. */
-    {
-      name: 'Mobile Chrome',
-      use: { ...devices['Pixel 5'] },
-    },
-    {
-      name: 'Mobile Safari',
-      use: { ...devices['iPhone 12'] },
-    },
+    // {
+    //   name: 'Mobile Chrome',
+    //   use: { ...devices['Pixel 5'] },
+    // },
+    // {
+    //   name: 'Mobile Safari',
+    //   use: { ...devices['iPhone 12'] },
+    // },
 
     /* Test against branded browsers. */
-    {
-      name: 'Microsoft Edge',
-      use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    },
-    {
-      name: 'Google Chrome',
-      use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    },
+    // {
+    //   name: 'Microsoft Edge',
+    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
+    // },
+    // {
+    //   name: 'Google Chrome',
+    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
+    // },
   ],
 
   /* Run your local dev server before starting the tests */
---
+++ tests/UIBasics.spec.ts
@@ -0,0 +1,31 @@
+import {test, expect} from '@playwright/test'
+
+test("Fill and validate the textbox", async ({page})=>{
+    // launch the url of the application - https://practicetestautomation.com/practice-test-login/
+   // goto("url") - https://
+   await page.goto("https://practicetestautomation.com/practice-test-login/")
+   // input#username
+   // fill(value) - fill the textbox with value
+   // pressSequentially(value) 
+   await page.locator('input#username').fill("student")
+   // toHaveValue(value) - validate the value of the textbox
+   await expect(page.locator('input#username')).toHaveValue("student")
+
+   await page.getByLabel("Password").fill("Password123")
+   await expect(page.getByLabel("Password")).toHaveValue("Password123")
+
+   // click on the submit button
+   // click() - click on the element
+ 
+   await page.getByRole('button', {name: 'Submit'}).click()
+   //toBeVisible() - Validate the visibility of an element on the webpage
+   await expect(page.locator("h1.post-title")).toBeVisible()
+
+   // toHaveText(text) - Validate the locators have the text value as text
+   // toContainText(text) - Validate if the element contains a text 
+   await expect(page.locator("h1.post-title")).toHaveText("Logged In Successfully")
+   await expect(page.locator("h1.post-title")).toContainText("Logged In")
+
+
+
+})
