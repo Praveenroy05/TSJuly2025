923afc71d8beb4fd876aef21ebb549cf4a0ed37c
Praveen <praveen.k@xnara.com>
2025-09-01 20:13:10 +0530

    added api

---
+++ tests/api.spec.ts
@@ -0,0 +1,45 @@
+// API - Application Programming Interface
+
+// Web application
+
+// Frontend - JS/TS, Angular, Ajax, React, Vue
+// Backend  - Java/Python/Php/.Net
+// Database - SQL, MySQL, PostgreS. MongoDB
+
+
+// GET, POST, PUT, PATCH and DELETE
+
+
+// Request Format
+    // URL - Uniform Resource Locator - 
+    // Base URL - domain  - https://rahulshettyacademy.com 
+    // end point - service - /api/ecom/auth/login
+    // HTTP method - GET, POST, PUT, DELETE
+    // Request Headers - Meta Data - Additional Information - {key : value}
+    // Body - Payload - JSON/XML/Form Data
+
+
+// Response Format
+    // Status code - 200, 201, 400, 500
+    // Response Headers - Key: value
+    // Response body - JSON
+
+
+    // URL - https://rahulshettyacademy.com/api/ecom/auth/login
+    // Method - POST
+    // Body - Payload - {"userEmail":"testnHNk@gmail.com","userPassword":"Testing@1234"}
+
+    // Status Code - 200 
+    // Response body - 
+
+    /*
+
+    {
+    "token": "hgfhgfhg",
+    "userId": "6674654357jhgfgd454456",
+    "message": "Login Successfully"
+    }
+
+    */
+
+
